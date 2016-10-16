Correct the backpropagation

DataExtraction/Extraction.py
@@ -16,29 +16,27 @@ def extractImagesLabels(labelsFileName, imagesFileName, normalization = True):
 	Functions which creates matrices of images and labels by extracting the content
 	of the given files
 	"""
-	imagesFile = gzip.open(imagesFileName,"rb")
-	imagesHeader = struct.unpack(">4L", imagesFile.read(struct.calcsize(">4L")))
+	with gzip.open(imagesFileName,"rb") as imagesFile, gzip.open(labelsFileName,"rb") as labelsFile :
+		imagesHeader = struct.unpack(">4L", imagesFile.read(struct.calcsize(">4L")))
+		labelsHeader = struct.unpack(">2L", labelsFile.read(struct.calcsize(">2L")))
 
-	labelsFile = gzip.open(labelsFileName,"rb")
-	labelsHeader = struct.unpack(">2L", labelsFile.read(struct.calcsize(">2L")))
+		magic, nimages, height, width = imagesHeader
+		magic, nlabels = labelsHeader
 
-	magic, nimages, height, width = imagesHeader
-	magic, nlabels = labelsHeader
+		imagesRes = []
+		labelsRes = []
 
-	imagesRes = []
-	labelsRes = []
-
-	for n in range(nimages):
-		label = np.zeros((10, 1))
-		label[int.from_bytes(labelsFile.read(1),byteorder='big')] = 1.0
-		if normalization:
-			img = np.array([(int.from_bytes(imagesFile.read(1),byteorder='big'))/256 - 0.5
-				for i in range(height * width)])
-		else:
-			img = np.array([(int.from_bytes(imagesFile.read(1),byteorder='big'))
-				for i in range(height * width)])
-		imagesRes.append(img.reshape((784,1)))
-		labelsRes.append(label)
+		for n in range(nimages):
+			label = np.zeros((10, 1))
+			label[int.from_bytes(labelsFile.read(1),byteorder='big')] = 1.0
+			if normalization:
+				img = np.array([(int.from_bytes(imagesFile.read(1),byteorder='big'))/256 - 0.5
+					for i in range(height * width)])
+			else:
+				img = np.array([(int.from_bytes(imagesFile.read(1),byteorder='big'))
+					for i in range(height * width)])
+			imagesRes.append(img.reshape((784,1)))
+			labelsRes.append(label)
 
 	return np.array(labelsRes), np.array(imagesRes)
 

NeuralNetwork/Network.py
@@ -23,6 +23,7 @@ class NeuralNetwork:
 		self.layersNumber = len(dims) - 1
 		self.weights = []
 		self.biases = []
+		np.random.seed(42)
 		for d in range(self.layersNumber):
 			self.weights.append(np.random.randn(dims[d+1], dims[d]))
 			self.biases.append(np.random.randn(dims[d+1], 1))
@@ -47,7 +48,7 @@ class NeuralNetwork:
 		error, pastError = 0, 0
 		for iteration in range(maxIteration):
 			# Decrease the learningRate
-			if error > pastError :
+			if iteration > 1 and error > pastError :
 				learningRate /= 2
 			pastError = error
 
@@ -65,10 +66,10 @@ class NeuralNetwork:
 				# Computes the difference for each batch
 				for i in range(batch*batchSize,(batch+1)*batchSize):
 					diffWeight, diffBias, diffError = self.computeDiff(inputs[i], targets[i])
-					totalDiffWeight = [totalDiffWeight[i] + diffWeight[i]
-										for i in range(len(totalDiffWeight))]
-					totalDiffBias = [totalDiffBias[i] + diffBias[i]
-										for i in range(len(totalDiffBias))]
+					totalDiffWeight = [totalDiffWeight[j] + diffWeight[j]
+										for j in range(len(totalDiffWeight))]
+					totalDiffBias = [totalDiffBias[j] + diffBias[j]
+										for j in range(len(totalDiffBias))]
 					error += diffError
 
 				# Update weights and biases of each neuron
@@ -103,10 +104,10 @@ class NeuralNetwork:
 		delta = dCost(lastRes, target) * dActivation(lastRes)
 		diffBias[-1] = delta
 		diffWeight[-1] = np.dot(delta, layerAct[-2].transpose())
-		for layer in reversed(range(1, self.layersNumber-1)):
+		for layer in reversed(range(self.layersNumber-1)):
 			delta = np.dot(self.weights[layer+1].transpose(), delta) *\
 				dActivation(layerSum[layer])
 			diffBias[layer] = delta
-			diffWeight[layer] = np.dot(delta, layerAct[layer-1].transpose())
+			diffWeight[layer] = np.dot(delta, layerAct[layer].transpose())
 
 		return diffWeight, diffBias, diffError