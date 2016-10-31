5cbd7695f999837aeab2a6fae73ad32b1ad7d6fe
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2016-10-31 21:23:47 +0100

    Update the library function to facilitate the autoencoder

--- MNIST_Analysis.py
+++ MNIST_Analysis.py
@@ -11,11 +11,28 @@ import scipy.misc
 import NeuralNetwork.Network as Network
 import DataExtraction.Extraction as Extraction
 
+minZero = 0.00000000000001
+maxOne = 0.9999999999999
+
+zero = np.array([maxOne,minZero,minZero,minZero,minZero,minZero,minZero,minZero,minZero,minZero]).reshape((10,1))
+one = np.array([minZero,maxOne,minZero,minZero,minZero,minZero,minZero,minZero,minZero,minZero]).reshape((10,1))
+two = np.array([minZero,minZero,maxOne,minZero,minZero,minZero,minZero,minZero,minZero,minZero]).reshape((10,1))
+three = np.array([minZero,minZero,minZero,maxOne,minZero,minZero,minZero,minZero,minZero,minZero]).reshape((10,1))
+four = np.array([minZero,minZero,minZero,minZero,maxOne,minZero,minZero,minZero,minZero,minZero]).reshape((10,1))
+five = np.array([minZero,minZero,minZero,minZero,minZero,maxOne,minZero,minZero,minZero,minZero]).reshape((10,1))
+six = np.array([minZero,minZero,minZero,minZero,minZero,minZero,maxOne,minZero,minZero,minZero]).reshape((10,1))
+seven = np.array([minZero,minZero,minZero,minZero,minZero,minZero,minZero,maxOne,minZero,minZero]).reshape((10,1))
+eight = np.array([minZero,minZero,minZero,minZero,minZero,minZero,minZero,minZero,maxOne,minZero]).reshape((10,1))
+nine = np.array([minZero,minZero,minZero,minZero,minZero,minZero,minZero,minZero,minZero,maxOne]).reshape((10,1))
+
 def test(network, images, targets):
-	res = 0
+	number = 0
+	res = []
 	for i in range(len(images)):
-		res += int(np.argmax(network.compute(images[i])) == np.argmax(targets[i]))
-	return res
+		imageRes = network.compute(images[i])
+		res.append(imageRes)
+		number += int(np.argmax(imageRes) == np.argmax(targets[i]))
+	return number, np.array(res)
 
 def dataExtraction():
 	print("Download")
@@ -42,16 +59,23 @@ def analysis(layers, learningRate, batchSize, iteration, probabilistic,
 	net.backpropagation(training_images, training_labels, learningRate, batchSize,
 		probabilistic, iteration)
 
+	numberTrain, outputTrain = test(net, training_images, training_labels)
+	numberTest, outputTest = test(net, testing_images, testing_labels)
 	print("Test")
-	print("On the training set : {} / {}".format(
-		test(net, training_images, training_labels), len(training_labels)))
-	print("On the testing set : {} / {}".format(
-		test(net, testing_images, testing_labels), len(testing_labels)))
+	print("On the training set : {} / {}".format(numberTrain, len(training_labels)))
+	print("On the testing set : {} / {}".format(numberTest, len(testing_labels)))
 
-	return net
+	return net, outputTrain, outputTest
 
-def computeImage(net, target, imageFile, x = 28, y = 28):
+def computeReverseImage(net, target, imageFile, x = 28, y = 28):
 	image = net.reverseCompute(target).reshape(x,y)
+	# Reverse extraction phasis
+	image = image* 255
+	scipy.misc.imsave(imageFile, image)
+
+def computeImage(net, input, imageFile, x = 28, y = 28):
+	print(net.compute(input).shape)
+	image = net.compute(input).reshape(x,y)
 	# Reverse extraction phasis
 	image = image* 255
 	scipy.misc.imsave(imageFile, image)
